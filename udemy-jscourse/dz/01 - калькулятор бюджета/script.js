355006e69490974f34cbef7d076358513bfa926a
Danil Kozlov <megacokes@gmail.com>
2019-04-29 19:52:44 +0500

    prettify

--- udemy-jscourse/dz/01 - калькулятор бюджета/script.js
+++ udemy-jscourse/dz/01 - калькулятор бюджета/script.js
@@ -1,5 +1,5 @@
 let money = prompt("Ваш бюджет на месяц"),
-    time = prompt("Введите дату в формате YYYY-MM-DD");
+  time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
   budget: money,
@@ -8,14 +8,14 @@ let appData = {
   optionalExpenses: {},
   income: [],
   savings: false
-}
+};
 
 appData.expenses.answer1 = answer2;
 appData.expenses.answer3 = answer4;
 
 for (i = 0; i < 2; i++) {
   let answer1 = prompt("Введите обязательную статью расходов в этом месяце"),
-      answer2 = prompt("Во сколько обойдется?");
+    answer2 = prompt("Во сколько обойдется?");
 }
 
 alert(appData.budget / 30);
