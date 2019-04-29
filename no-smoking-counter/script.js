d33a7a6963fd7d4845930dcdac3264ca1bcef1b1
Danil Kozlov <megacokes@gmail.com>
2019-04-29 21:46:51 +0500

    no smoking counter v0.5

--- no-smoking-counter/script.js
+++ no-smoking-counter/script.js
@@ -2,21 +2,32 @@
 // цены на https://www.tabacum.ru/info/cigarette/brand/WINSTON
 
 var lastTime = new Date("September 26, 2018"),
-    currentDate = new Date(),
-    noSmokingTime = (currentDate.getTime() - lastTime.getTime()) / (1000 * 60 * 60 * 24),
-    cigCost = (115 + (107.25 + 143) / 2) / 2,
-    savedMoney = noSmokingTime * cigCost;
+  currentDate = new Date(),
+  noSmokingTime =
+    (currentDate.getTime() - lastTime.getTime()) / (1000 * 60 * 60 * 24),
+  cigCost = (115 + (107.25 + 143) / 2) / 2,
+  startingCosts = 2990 + 1990 + 390 * 2 + 100,
+  zhizhCosts = 500,
+  tempedCosts = 650 + 200 + 270 + 690 + 200 + 200 + 250 + 250,
+  savedMoney = noSmokingTime * cigCost;
 
-console.log("Не курю " + Math.round(noSmokingTime) +" дней");
+console.log("Не курю " + Math.round(noSmokingTime) + " дней");
 
-console.log("Сэкономил ~" + savedMoney.toFixed(2) + " руб.");
+console.log("Экономия на сигах ~" + savedMoney.toFixed(2) + " руб.");
 
-console.log("Сэкономил с учётом стартового дерьма ~" +
-            (savedMoney - 2990 - 1990 - 390 * 2 - 100).toFixed(2) + " руб.");
+console.log("Стартовые расходы ~" + startingCosts.toFixed(2) + " руб.");
 
-console.log("Сэкономил с учётом жыжы ~" + (savedMoney -
-            noSmokingTime / 30 * 500).toFixed(2) + " руб.");
+console.log("Расходы на жыжу в месяц ~" + zhizhCosts + " руб.");
 
-console.log("Сэкономил с учётом жыжы и стартового дерьма ~" + (savedMoney -
-            noSmokingTime / 30 * 500 - 2990 - 1990 - 390 * 2 -
-            100).toFixed(2)) + " руб.";
+console.log("Расходы на всякую поебень ~" + tempedCosts.toFixed(2));
+
+console.log(
+  "Сэкономил с учётом жыжы и стартового дерьма ~" +
+    (
+      savedMoney -
+      startingCosts -
+      tempedCosts -
+      zhizhCosts * (noSmokingTime / 30)
+    ).toFixed(2) +
+    " руб."
+);
