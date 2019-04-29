ec6eef932fbe53dafe8fe3944a6b4e72400ef36d
Danil Kozlov <megacokes@gmail.com>
2019-04-29 20:41:22 +0500

    Udemy функции

--- udemy-jscourse/07 Функции/script.js
+++ udemy-jscourse/07 Функции/script.js
@@ -1,1 +1,17 @@
 // Функции
+
+function showFirstMessage(text) {
+  alert(text);
+}
+
+showFirstMessage("Hello World!");
+
+// Вызываем функцию с разными параметрами
+
+function calc(a, b) {
+  return a + b;
+}
+
+console.log(calc(7, 3.368));
+console.log(calc(-7, 3.368));
+console.log(calc(-7, 0.368));
