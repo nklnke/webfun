7b4c9e5c5dc287835974ceba003c88387a60e954
Danil Kozlov <megacokes@gmail.com>
2019-04-30 19:46:10 +0500

    udemyjs Функции

--- udemy-jscourse/07 Функции/script.js
+++ udemy-jscourse/07 Функции/script.js
@@ -15,3 +15,24 @@ function calc(a, b) {
 console.log(calc(7, 3.368));
 console.log(calc(-7, 3.368));
 console.log(calc(-7, 0.368));
+
+console.log("");
+
+// return также возвращает переменную во "внешний мир"
+
+function returnVar() {
+  let num = 50;
+  return num;
+}
+
+let anotherNum = returnVar();
+console.log(anotherNum);
+
+console.log("");
+
+// Современный стандарт объявления функций ES6
+
+let calcES6 = (c, d) => c + d;
+
+console.log(calcES6(5, 15));
+console.log(calcES6(666, 0.154));
