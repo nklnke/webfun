fd226ef00e669b9887a1b85250048b5acf1ca71c
Danil Kozlov <megacokes@gmail.com>
2019-04-30 19:57:25 +0500

    udemyjs Методы, свойства

---
+++ udemy-jscourse/08 Методы, свойства/script.js
@@ -0,0 +1,19 @@
+// Для строковых данных
+
+let string = "test";
+
+console.log(string.length);
+console.log(string.toUpperCase());
+
+console.log("");
+
+let twelve = "12.2";
+
+console.log(Math.round(twelve));
+
+// Для числовых данных
+
+let width = "300.7px";
+
+console.log(parseInt(width));
+console.log(parseFloat(width));
