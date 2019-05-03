a3cc6e6500fc80065cfb065ad729a38062fa3249
Danil Kozlov <megacokes@gmail.com>
2019-05-03 19:01:56 +0500

    udemy js ДЗ вопросы

---
+++ udemy-jscourse/dz/02 Shiet/script.js
@@ -0,0 +1,25 @@
+let x = 5,
+  a,
+  b;
+
+console.log(x++);
+console.log([] + false - null + true);
+
+let y = 1;
+let z = (y = 2);
+console.log(z);
+
+console.log([] + 1 + 2);
+
+console.log("1"[0]);
+
+console.log(2 && 1 && null && 0 && undefined);
+
+console.log(!!(a && b));
+console.log(a && b);
+
+console.log(null || (2 && 3) || 4);
+
+console.log(+"Infinity");
+
+console.log(0 || "" || 2 || undefined || true || falsе);
