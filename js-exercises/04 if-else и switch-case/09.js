f9d5e2ee4424c15576d0a9124e62b626eb65c27d
Danil Kozlov <megacokes@gmail.com>
2019-05-10 22:17:29 +0500

    js-exercises 04 / 09-10

--- js-exercises/04 if-else и switch-case/09.js
+++ js-exercises/04 if-else и switch-case/09.js
@@ -1,1 +1,15 @@
-// 
+// Если переменная test равна true, то выведите 'Верно', иначе выведите 'Неверно'. Проверьте работу скрипта при test, равном true, false. Напишите два варианта скрипта - с короткой записью и с длинной.
+
+var test = true;
+
+if (test == true) {
+  console.log("true!");
+} else {
+  console.log("false!");
+}
+
+if (test) {
+  console.log("true!");
+} else {
+  console.log("false!");
+}
