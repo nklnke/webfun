1c307156ece8ade958d15e36f935725aff9b93ca
Danil Kozlov <megacokes@gmail.com>
2019-05-10 16:30:23 +0500

    js-exercises 03 / 07-10

--- js-exercises/03 Основы работы с массивами и объектами/10.js
+++ js-exercises/03 Основы работы с массивами и объектами/10.js
@@ -1,1 +1,11 @@
-// 
+// Дан объект {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}. Выведите с его помощью слово 'jQuery'.
+
+var obj = {
+  js: ["jQuery", "Angular"],
+  php: "Hello",
+  css: "World"
+};
+
+console.log(obj.js[0]);
+console.log(obj["js"][0]);
+console.log(obj.php + " " + obj.css);
