e9e080c95f3dca9661a8088eb9b6db4b0a9210b6
Danil Kozlov <megacokes@gmail.com>
2019-05-12 23:11:58 +0500

    js-exercises 04 / 16-17

--- js-exercises/04 if-else и switch-case/17.js
+++ js-exercises/04 if-else и switch-case/17.js
@@ -1,1 +1,20 @@
-// 
+// В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+
+var month = 7,
+  season;
+
+if (month == 12 || month <= 2) {
+  season = "zima";
+}
+
+if (month > 3 && month <= 5) {
+  season = "vesna";
+}
+
+if (month > 6 && month <= 8) {
+  season = "leto";
+}
+
+if (month >= 9 && month <= 11) {
+  season = "vesna";
+}
