750cccbfaf36a40b7fa01caefdd94095fc0f8482
Danil Kozlov <megacokes@gmail.com>
2019-05-12 23:01:29 +0500

    js-exercises 04 / 15

--- js-exercises/04 if-else и switch-case/15.js
+++ js-exercises/04 if-else и switch-case/15.js
@@ -1,1 +1,21 @@
-// 
+// Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет значение '1', то в переменную result запишем 'зима', если имеет значение '2' – 'весна' и так далее. Решите задачу через switch-case.
+
+var num = 2,
+  result;
+
+switch (num) {
+  case 1:
+    result = "Зима";
+    break;
+  case 2:
+    result = "Vesna";
+    break;
+  case 3:
+    result = "Leto";
+    break;
+  case 4:
+    result = "Ostn'";
+    break;
+}
+
+console.log(result);
