6aa2b39b2c51d3e0e0cfdc45478155a65273702f
Danil Kozlov <megacokes@gmail.com>
2019-05-13 13:20:27 +0500

    timer.js update

--- test/timer.js
+++ test/timer.js
@@ -1,11 +1,15 @@
+// Жизнь таймеру обеспечивает функция update, вызываемая каждую секунду
+
 function update() {
   var date = new Date();
   console.log(date);
 }
 
+// Функция start для запуска таймера
+
 function start() {
   setInterval(update, 1000);
-  update(); // <--  начать тут же, не ждать 1 секунду, пока setInterval сработает
+  update(); // начать тут же, не ждать 1 секунду, пока setInterval сработает
 }
 
 start();
