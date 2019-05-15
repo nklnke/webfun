7d6a13bb82219eaf9cc51cabd16242282e420261
Danil Kozlov <megacokes@gmail.com>
2019-05-15 19:16:36 +0500

    js-exercises 04 / 18-21

--- js-exercises/04 if-else и switch-case/18.js
+++ js-exercises/04 if-else и switch-case/18.js
@@ -1,1 +1,15 @@
-// 
+// Дана строка, состоящая из символов, например, 'abcde'. Проверьте, что первым символом этой строки является буква 'a'. Если это так - выведите 'да', в противном случае выведите 'нет'.
+
+var stroke = "abcde";
+
+if (stroke.charAt(0) == "a") {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+if (stroke[0] == "a") {
+  console.log("true");
+} else {
+  console.log("false");
+}
--- js-exercises/04 if-else и switch-case/21.js
+++ js-exercises/04 if-else и switch-case/21.js
@@ -1,1 +1,15 @@
-// 
+// Дана строка из 6-ти цифр. Проверьте, что сумма первых трех цифр равняется сумме вторых трех цифр. Если это так - выведите 'да', в противном случае выведите 'нет'.
+
+var stroke = "123456",
+  sum1 = Number(stroke[0]) + Number(stroke[1]) + Number(stroke[2]),
+  sum2 = Number(stroke[3]) + Number(stroke[4]) + Number(stroke[5]);
+
+if (sum1 == sum2) {
+  console.log("true");
+  console.log("sum1 = " + sum1);
+  console.log("sum2 = " + sum2);
+} else {
+  console.log("false");
+  console.log("sum1 = " + sum1);
+  console.log("sum2 = " + sum2);
+}
