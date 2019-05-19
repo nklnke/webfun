e7cb393246161adb17819b84edbba4b292c52220
Danil Kozlov <megacokes@gmail.com>
2019-05-19 20:08:34 +0500

    Update 01.js

--- js-exercises/09 практика/01.js
+++ js-exercises/09 практика/01.js
@@ -1,5 +1,16 @@
 // Дана строка. Сделайте заглавным первый символ этой строки не используя цикл. Найдите два решения.
 
-var stroke = "Дана строка";
+var stroke = "дана строка";
+
+// 1
 
 console.log(stroke[0].toUpperCase() + stroke.slice(1));
+
+// 2
+
+stroke = stroke.split("");
+stroke[0] = stroke[0].toUpperCase();
+
+var mass = stroke.join("");
+
+console.log(mass);
