de8befaa60b18cf08c76e08e7632cf53ceadfa56
Danil Kozlov <megacokes@gmail.com>
2019-05-19 19:32:55 +0500

    js-exercises 08 / 11-

--- js-exercises/08 массивы/12.js
+++ js-exercises/08 массивы/12.js
@@ -1,1 +1,11 @@
-// 
+// Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].
+
+var mass = [1, 2, 3, 4, 5];
+
+mass.splice(1, 0, "a", "b");
+
+console.log(mass);
+
+mass2 = mass.splice(3, 0, "c");
+
+console.log(mass2);
