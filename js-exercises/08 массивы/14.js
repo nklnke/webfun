c6354fccc73f26373e0c86f8b892c39da3726760
Danil Kozlov <megacokes@gmail.com>
2019-05-19 19:58:21 +0500

    js-exercises 08 / 13-14

--- js-exercises/08 массивы/14.js
+++ js-exercises/08 массивы/14.js
@@ -1,1 +1,9 @@
-// 
+// Дан объект {js:'test', jq: 'hello', css: 'world'}. Получите массив его ключей.
+
+var obj = {
+  js: "test",
+  jq: "hello",
+  css: "world"
+};
+
+console.log(Object.keys(obj));
