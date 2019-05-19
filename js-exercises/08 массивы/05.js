bf00ed642f09f28aa24ef08e0b74ff1541d49398
Danil Kozlov <megacokes@gmail.com>
2019-05-19 19:16:45 +0500

    js-exercises 08 / 03-10

--- js-exercises/08 массивы/05.js
+++ js-exercises/08 массивы/05.js
@@ -1,1 +1,7 @@
-// 
+// Дан массив ['js', 'css', 'jq']. Выведите на экран первый элемент.
+
+var mass = ["js", "css", "jq"],
+  a = mass.shift();
+
+console.log(a);
+console.log(mass);
--- js-exercises/08 массивы/06.js
+++ js-exercises/08 массивы/06.js
@@ -1,1 +1,7 @@
-// 
+// Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент.
+
+var mass = ["js", "css", "jq"],
+  a = mass.pop();
+
+console.log(a);
+console.log(mass);
