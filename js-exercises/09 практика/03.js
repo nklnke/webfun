5c0e702933b211c62a6f77b403ec6ae6869b2c50
Danil Kozlov <megacokes@gmail.com>
2019-05-20 19:26:27 +0500

    js-exercises 09 / 02-4

--- js-exercises/09 практика/03.js
+++ js-exercises/09 практика/03.js
@@ -1,1 +1,12 @@
-// 
+// Проверьте, что строка начинается на http://.
+
+var stroke =
+  "http://code.mu/tasks/javascript/base/praktika-na-kombinacii-standartnyh-funkcij-javascript.html";
+
+if (stroke.slice(0, 7) == "http://") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+console.log(stroke);
--- js-exercises/09 практика/04.js
+++ js-exercises/09 практика/04.js
@@ -1,1 +1,12 @@
-// 
+// Проверьте, что строка заканчивается на .html.
+
+var stroke =
+  "http://code.mu/tasks/javascript/base/praktika-na-kombinacii-standartnyh-funkcij-javascript.html";
+
+if (stroke.substr(-5) == ".html") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+console.log(stroke);
