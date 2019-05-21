d5b032e208b65394fff373567897464a3a37abe2
Danil Kozlov <megacokes@gmail.com>
2019-05-21 22:01:07 +0500

    14 / 05

--- js-exercises/14 задачи на массивы/05.js
+++ js-exercises/14 задачи на массивы/05.js
@@ -1,1 +1,11 @@
-// 
+// Дан массив с числами. Не используя метода reverse,
+// переверните его элементы в обратном порядке.
+
+var mass = [1, 2, 3, 4, 5],
+  newMass = [];
+
+for (let i = mass.length - 1; i >= 0; i--) {
+  newMass.push(mass[i]);
+}
+
+console.log(newMass);
