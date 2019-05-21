adaace5b354ac4fc1b65b5e61ebde6b35b7301b7
Danil Kozlov <megacokes@gmail.com>
2019-05-21 20:12:36 +0500

    13 / 04-06

--- js-exercises/13 приёмы работы с циклами/06.js
+++ js-exercises/13 приёмы работы с циклами/06.js
@@ -1,1 +1,23 @@
-// 
+// Нарисуйте пирамиду, как показано на рисунке, воспользовавшись циклом for:
+
+// xx
+// xxxx
+// xxxxxx
+// xxxxxxxx
+// xxxxxxxxxx
+
+var stroke = "";
+
+for (let i = 0; i < 5; i++) {
+  stroke += "xx";
+  console.log(stroke);
+}
+
+// Для подключения скрипта к странице:
+
+// var stroke = "xx";
+
+// for (let i = 0; i < 5; i++) {
+//   document.write(stroke + "<br>");
+//   stroke += "xx";
+// }
