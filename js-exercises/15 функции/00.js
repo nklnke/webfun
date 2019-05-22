3eee2399c3410e7d67f856be1494fce90441b7a4
Danil Kozlov <megacokes@gmail.com>
2019-05-22 19:13:12 +0500

    15/ filestarter

---
+++ js-exercises/15 функции/00.js
@@ -0,0 +1,25 @@
+// Дан массив с числами.
+// Создайте из него новый массив, где останутся лежать
+// только положительные числа. Создайте для этого
+// вспомогательную функцию isPositive(), которая
+// параметром будет принимать число и возвращать true,
+// если число положительное, и false - если отрицательное.
+
+var arr = [1, 2, 3, -1, -2, -3];
+
+function isPositive(num) {
+  if (num >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var newArr = [];
+for (var i = 0; i <= arr.length; i++) {
+  if (isPositive(arr[i])) {
+    newArr.push(arr[i]);
+  }
+}
+
+console.log(newArr);
