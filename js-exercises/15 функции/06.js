e0981bfeea2a95bdc532df101f84b60a16df74c6
Danil Kozlov <megacokes@gmail.com>
2019-05-23 19:27:09 +0500

    15 / 05-07

--- js-exercises/15 функции/06.js
+++ js-exercises/15 функции/06.js
@@ -1,1 +1,23 @@
-// 
+// Дан массив с целыми числами. Создайте из него новый массив,
+// где останутся лежать только четные из этих чисел.
+// Для этого используйте вспомогательную функцию isEven
+// из предыдущей задачи.
+
+var mass = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12],
+  newMass = [];
+
+function isEven(num) {
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+for (let i = 0; i < mass.length; i++) {
+  if (isEven(mass[i])) {
+    newMass.push(mass[i]);
+  }
+}
+
+console.log(newMass);
--- js-exercises/15 функции/07.js
+++ js-exercises/15 функции/07.js
@@ -1,1 +1,18 @@
-// 
+// Сделайте функцию getDivisors, которая параметром принимает
+// число и возвращает массив его делителей
+// (чисел, на которое делится данное число).
+
+var mass = [];
+
+function getDivisors(num) {
+  for (let i = 1; i < 10; i++) {
+    if (num % i == 0) {
+      mass.push(i);
+    }
+  }
+  console.log(mass);
+}
+
+// getDivisors(9);
+getDivisors(8);
+// getDivisors(4);
