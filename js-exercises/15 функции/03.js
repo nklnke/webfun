11e92d34e44819aa56d99994d07e7fd38f6c92b6
Danil Kozlov <megacokes@gmail.com>
2019-05-23 19:21:21 +0500

    15 / 03 - function getDigitsSum update

--- js-exercises/15 функции/03.js
+++ js-exercises/15 функции/03.js
@@ -2,14 +2,17 @@
 // которая параметром принимает целое число и возвращает
 // сумму его цифр.
 
-var sum = 0;
-
 function getDigitsSum(num) {
-  var string = String(num);
+  var sum = 0;
+  let string = String(num);
   for (let i = 0; i < string.length; i++) {
     sum += Number(string[i]);
   }
-  console.log(sum);
+  return sum;
 }
 
+console.log(getDigitsSum(22222222));
+console.log(getDigitsSum(22222222));
+console.log(getDigitsSum(22222222));
+getDigitsSum(22222222);
 getDigitsSum(22222222);
