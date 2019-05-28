3cde7def4d9a6a41d841fb97333002695fdb33c2
Danil Kozlov <megacokes@gmail.com>
2019-05-28 15:27:28 +0500

    17 / 01-02

--- js-exercises/17 продвинутые функции, рекурсия/01.js
+++ js-exercises/17 продвинутые функции, рекурсия/01.js
@@ -3,3 +3,14 @@
 
 var mass = [1, 2, 3, 4, 5, 6];
 
+function print(mass) {
+  console.log(mass[0]);
+
+  mass.splice(0, 1);
+
+  if (mass.length > 0) {
+    print(mass);
+  }
+}
+
+print(mass);
--- js-exercises/17 продвинутые функции, рекурсия/02.js
+++ js-exercises/17 продвинутые функции, рекурсия/02.js
@@ -1,1 +1,23 @@
-// 
+// Дано число. Сложите его цифры.
+// Если сумма получилась более 9-ти, опять сложите его цифры.
+// И так, пока сумма не станет однозначным числом (9 и менее).
+
+var num = 367,
+  sum = 0;
+
+function shazam(num) {
+  let mass = num.toString().split("");
+  console.log(mass);
+
+  for (let i = 0; i < mass.length; i++) {
+    sum += +mass[i];
+    // или sum += Number(mass[i]);
+
+    if (sum > 9) {
+      sum -= +mass[i];
+      return sum;
+    }
+  }
+}
+
+console.log(shazam(num));
