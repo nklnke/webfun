0d9f9588109766f0ef22630728cdc320be90bf00
Danil Kozlov <megacokes@gmail.com>
2019-05-28 16:14:21 +0500

    18 / 04-08

--- js-exercises/18 отработка циклов/04.js
+++ js-exercises/18 отработка циклов/04.js
@@ -1,1 +1,10 @@
-// 
+// Заполните массив 10-ю иксами с помощью цикла.
+
+var mass = [];
+
+for (let i = 0; i < 10; i++) {
+  mass.push("x");
+  // mass[i] = "x";
+}
+
+console.log(mass);
--- js-exercises/18 отработка циклов/05.js
+++ js-exercises/18 отработка циклов/05.js
@@ -1,1 +1,10 @@
-// 
+// Заполните массив числами от 1 до 10 с помощью цикла.
+
+var mass = [];
+
+for (let i = 1; i <= 10; i++) {
+  mass.push(i);
+  // mass[i] = i;
+}
+
+console.log(mass);
