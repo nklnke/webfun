4130a6e8068d177515dd4cbd1d18ab71fba285ff
Danil Kozlov <megacokes@gmail.com>
2019-05-28 13:56:53 +0500

    16 / 04

--- js-exercises/16 практика на функции/04.js
+++ js-exercises/16 практика на функции/04.js
@@ -1,1 +1,18 @@
-// 
+// Дана строка, например, '123456'. Сделайте из нее '214365'.
+
+var stroke = "123456";
+
+function slicer() {
+  let mass = stroke.split(""), // Не забывать, бл, про кавычки в сплите ("") !
+    newMass = [];
+
+  for (let i = 0; i < mass.length; i += 2) {
+    let temp = mass.slice(i, i + 2).reverse(); // маленький массив из след. 2х элеметов, поменянных местами
+    newMass = newMass.concat(temp); // push() тут не подходит
+  }
+
+  stroke = newMass.join("");
+  return stroke;
+}
+
+console.log(slicer(stroke));
