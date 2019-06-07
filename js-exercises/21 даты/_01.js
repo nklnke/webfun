52b51c1ccd36ba972e7f5873452fd0b42701382c
Danil Kozlov <megacokes@gmail.com>
2019-06-07 13:32:45 +0500

    21 / _01-_02

---
+++ js-exercises/21 даты/_01.js
@@ -0,0 +1,5 @@
+// Выведите на экран текущие день, месяц и год в формате 'год-месяц-день'.
+
+var date = new Date();
+
+console.log(date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate());
