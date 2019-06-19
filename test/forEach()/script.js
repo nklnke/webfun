087852cf89710328c4a579b8fe559c62d2659981
Danil Kozlov <megacokes@gmail.com>
2019-06-19 19:26:53 +0500

    .forEach()

---
+++ test/forEach()/script.js
@@ -0,0 +1,19 @@
+let square = document.querySelectorAll(".square");
+
+// для querySelectorAll лучше использовать метод forEach(),
+// а не цикл. forEach() выполняет указанную функцию
+// один раз для каждого элемента в массиве
+
+square.forEach(element => {
+  element.style.backgroundColor = "gray";
+});
+
+square.forEach(function(elem, i, hearts) {
+  // каждый перебираемый элемент, i, передаваемый массив
+  // (но его нужно назвать по-другому)
+  elem.style.color = "red";
+});
+
+square.forEach(function(element) {
+  console.log(element);
+});
