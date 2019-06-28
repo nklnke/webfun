8104fee22e125f92a85036db777d5c2b957575b6
Danil Kozlov <megacokes@gmail.com>
2019-06-28 19:14:57 +0500

    Rename 24 folder

---
+++ udemy-jscourse/25 ES6. let, const/script.js
@@ -0,0 +1,21 @@
+// let, const
+// Spread/Rest
+// `${let}`
+// ()=>{}
+// Promise
+// Деструктуризация
+// for...of
+// Class
+
+// Интерполяция - более простой способ вставить
+// переменную/выражение.
+
+let name = "ivan",
+  age = 30,
+  mail = "mail@mail.ru";
+
+console.log("Пользователю " + name + " " + age + " лет. Его почта - " + mail);
+
+// vs
+
+console.log(`Пользователю ${name} ${age} лет. Его почта - ${mail}`);
