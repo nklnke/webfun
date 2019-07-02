60b9790dc555f6dafb1c33a5159f3a9ff563212f
Danil Kozlov <megacokes@gmail.com>
2019-07-02 19:25:00 +0500

    Update script.js

--- udemy-jscourse/31 AJAX/script.js
+++ udemy-jscourse/31 AJAX/script.js
@@ -1,7 +1,9 @@
-// AJAX (Asynchronous Javascript And Xml) – технология
-// обращения к серверу без перезагрузки страницы.
-// За счет этого уменьшается время отклика и
-// веб-приложение по интерактивности больше напоминает десктоп.
+/**
+ * AJAX (Asynchronous Javascript And Xml) – технология
+ * обращения к серверу без перезагрузки страницы.
+ * За счет этого уменьшается время отклика и
+ * веб-приложение по интерактивности больше напоминает десктоп.
+ */
 
 let inputRub = document.getElementById("rub"),
   inputUsd = document.getElementById("usd");
@@ -9,8 +11,9 @@ let inputRub = document.getElementById("rub"),
 inputRub.addEventListener("input", () => {
   let request = new XMLHttpRequest();
 
-  // request.open(method, url, async, login, pass);
   /**
+   * request.open(method, url, async, login, pass);
+   *
    * 5 аргументов:
    * 1) метод (н-р GET или POST) - метод, которым клиент общается с сервером
    * 2) путь к серверу (локальному, облачному)
@@ -22,19 +25,22 @@ inputRub.addEventListener("input", () => {
   request.open(
     "GET",
     "currency.json"
-    /**true стоит и так, login/pass не нужны */
+    // true стоит и так, login/pass не нужны
   );
   request.setRequestHeader("Content-type", "application/json", "charset=utf-8");
   request.send(); // request.send(body); body - тело запроса
 
-  // Ответы, которые может прислать сервер
-  // status - http-код ответа (404, 503, ...)
-  // statusText - текстовое описание ответа от сервера (ok или not found)
-  // responseText/response - текст ответа
-  // readyState - текущее состояние запроса (5 штук)
+  /**
+   * Ответы, которые может прислать сервер
+   * status - http-код ответа (404, 503, ...)
+   * statusText - текстовое описание ответа от сервера (ok или not found)
+   * responseText/response - текст ответа
+   * readyState - текущее состояние запроса (5 штук)
+   */
 
   // Обработчик событий следит за изменением ответа от сервера, а конкретно за изменением его состояния (readyState)
   request.addEventListener("readystatechange", function() {
+    // код ответа 200 указывает, что запрос выполнен успешно
     if (request.readyState === 4 && request.status == 200) {
       let data = JSON.parse(request.response); // Парсим ответ от сервера request.response, в котором JSON-данные
 
