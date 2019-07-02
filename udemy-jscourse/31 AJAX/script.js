9b3ebe2c9a189193aee2fec66a0164551d47649e
Danil Kozlov <megacokes@gmail.com>
2019-07-02 19:20:12 +0500

    Update script.js

--- udemy-jscourse/31 AJAX/script.js
+++ udemy-jscourse/31 AJAX/script.js
@@ -24,6 +24,23 @@ inputRub.addEventListener("input", () => {
     "currency.json"
     /**true стоит и так, login/pass не нужны */
   );
-});
+  request.setRequestHeader("Content-type", "application/json", "charset=utf-8");
+  request.send(); // request.send(body); body - тело запроса
 
-// 10:04
+  // Ответы, которые может прислать сервер
+  // status - http-код ответа (404, 503, ...)
+  // statusText - текстовое описание ответа от сервера (ok или not found)
+  // responseText/response - текст ответа
+  // readyState - текущее состояние запроса (5 штук)
+
+  // Обработчик событий следит за изменением ответа от сервера, а конкретно за изменением его состояния (readyState)
+  request.addEventListener("readystatechange", function() {
+    if (request.readyState === 4 && request.status == 200) {
+      let data = JSON.parse(request.response); // Парсим ответ от сервера request.response, в котором JSON-данные
+
+      inputUsd.value = inputRub.value / data.usd;
+    } else {
+      inputUsd.value = "SASAI";
+    }
+  });
+});
