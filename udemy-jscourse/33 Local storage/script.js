1252c0dc2d737bf5b851f53def1e6f339af4df17
Danil Kozlov <megacokes@gmail.com>
2019-07-03 19:47:29 +0500

    Update 33 Local storage

--- udemy-jscourse/33 Local storage/script.js
+++ udemy-jscourse/33 Local storage/script.js
@@ -1,9 +1,9 @@
-// localstorage - свойство window
-
-// ~5mb помещается
-
-// Инструменты разработчика - вкладка Application/local Storage
-
+/**
+ * localstorage - свойство window
+ * ~5mb помещается
+ * Инструменты разработчика - вкладка Application/local Storage
+ * Привязывается к домену
+ */
 console.log(window);
 
 localStorage.setItem("number", 1);
@@ -18,4 +18,47 @@ localStorage.removeItem("number");
 
 // localStorage.clear();
 
-// 06:20
+//////////////////////////////////
+
+window.addEventListener("DOMContentLoaded", function() {
+  let checkbox = document.getElementById("remember-me"),
+    change = document.getElementById("change"),
+    form = document.getElementsByTagName("form")[0];
+
+  /* Если один раз чекнуть remember me, то в local storage
+   * записывается свойство ckecked = true, и при обновлении
+   * страницы оно сохраняется.
+   */
+  if (localStorage.getItem("isChecked") === "true") {
+    checkbox.checked = true;
+  }
+
+  checkbox.addEventListener("click", function() {
+    localStorage.setItem("isChecked", true);
+  });
+
+  if (localStorage.getItem("bg") === "changed") {
+    form.style.backgroundColor = "red"; // здесь, чтобы красный фон записывался в local storage
+  }
+
+  change.addEventListener("click", function() {
+    localStorage.setItem("bg", "changed");
+    form.style.backgroundColor = "red"; // здесь, чтобы сразу после клика менялся фон формы
+  });
+});
+
+///////////////////////////////////////
+
+// Запись своих данных local storage
+
+let person = {
+  name: "Alex",
+  age: 25,
+  tech: ["mobile", "notebook"]
+};
+
+let serializedPerson = JSON.stringify(person);
+
+localStorage.setItem("Alex", serializedPerson);
+
+console.log(JSON.parse(localStorage.getItem("Alex")));
