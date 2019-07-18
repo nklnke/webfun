f07d076a9bc54c404c7899f59fb060c5db472e49
Danil Kozlov <megacokes@gmail.com>
2019-07-18 20:02:14 +0500

    Add modal window

--- udemy-jscourse/dz/06 Yoga/js/script.js
+++ udemy-jscourse/dz/06 Yoga/js/script.js
@@ -102,5 +102,36 @@ window.addEventListener("DOMContentLoaded", function() {
     updateClock(); // Запускаем таймер сразу же, не ждём 1 секунду
   }
 
+  // Запуск таймера
   setClock("timer", deadline); // timer - id таймера в документе
+
+  // Modal window
+  let more = document.querySelector(".more"),
+    descriptionBtn = document.querySelector(".description-btn"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close");
+
+  // event listenerы на кнопки "Узнать больше" в табах и далее по странице
+  descriptionBtn.addEventListener("click", function() {
+    overlay.style.display = "block";
+    this.classList.add("more-splash"); // Анимация кнопки
+
+    // Запрет прокрутки страницы при открытом модальном окне
+    document.body.style.overflow = "hidden";
+  });
+
+  more.addEventListener("click", function() {
+    overlay.style.display = "block";
+    this.classList.add("more-splash"); // Анимация кнопки
+
+    // Запрет прокрутки страницы при открытом модальном окне
+    document.body.style.overflow = "hidden";
+  });
+
+  close.addEventListener("click", function() {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash"); // Анимация кнопки
+
+    document.body.style.overflow = "";
+  });
 });
