d6ba6a53ddd1e1e7e33f935506d73e75ede11b4c
Danil Kozlov <megacokes@gmail.com>
2019-07-23 19:45:07 +0500

    Add calculator

--- udemy-jscourse/dz/06 Yoga/js/script.js
+++ udemy-jscourse/dz/06 Yoga/js/script.js
@@ -271,4 +271,40 @@ window.addEventListener("DOMContentLoaded", function() {
       }
     }
   });
+
+  // Calculator
+  let persons = document.querySelectorAll(".counter-block-input")[0],
+    restDays = document.querySelectorAll(".counter-block-input")[1],
+    place = document.getElementById("select"),
+    totalValue = document.getElementById("total");
+
+  let personSum = 0,
+    daysSum = 0,
+    total = 0;
+
+  totalValue.innerHTML = 0;
+
+  persons.addEventListener("change", function() {
+    personSum = +this.value;
+    total = (daysSum + personSum) * 4000;
+
+    if (restDays.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
+
+  restDays.addEventListener("change", function() {
+    daysSum = +this.value;
+    total = (daysSum + personSum) * 4000;
+
+    if (persons.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total;
+    }
+  });
 });
+
+// 10:25
