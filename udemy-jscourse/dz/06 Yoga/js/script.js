caaed4fd7794c6e5eb116af61d251591cedb7a16
Danil Kozlov <megacokes@gmail.com>
2019-07-25 19:01:26 +0500

    Update script.js

--- udemy-jscourse/dz/06 Yoga/js/script.js
+++ udemy-jscourse/dz/06 Yoga/js/script.js
@@ -284,27 +284,40 @@ window.addEventListener("DOMContentLoaded", function() {
 
   totalValue.innerHTML = 0;
 
+  // Кол-во человек
   persons.addEventListener("change", function() {
     personSum = +this.value;
     total = (daysSum + personSum) * 4000;
 
-    if (restDays.value == "") {
+    if (restDays.value == "" || persons.value == "") {
       totalValue.innerHTML = 0;
     } else {
       totalValue.innerHTML = total;
     }
   });
 
+  // Кол-во дней
   restDays.addEventListener("change", function() {
     daysSum = +this.value;
     total = (daysSum + personSum) * 4000;
 
-    if (persons.value == "") {
+    if (restDays.value == "" || persons.value == "") {
       totalValue.innerHTML = 0;
     } else {
       totalValue.innerHTML = total;
     }
   });
-});
 
-// 10:25
+  // Место отдыха
+  place.addEventListener("change", function() {
+    if (restDays.value == "" || persons.value == "") {
+      totalValue.innerHTML = 0;
+    } else {
+      let a = total;
+
+      // В зависимости от выбранного дропдауна меняется коэффициент
+      // (берётся из <option id="mumbai" value="1">Индия, Мумбай</option>)
+      totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+    }
+  });
+});
