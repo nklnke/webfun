b871d5421abb3fc8e1a1cafec651c42965adfd69
Danil Kozlov <megacokes@gmail.com>
2019-07-29 15:48:28 +0500

    Фильтр по тегам

---
+++ test/Фильтр по тегам/script.js
@@ -0,0 +1,25 @@
+/*
+  - Создаётся массив элементов с общим классом (all в данном случае)
+  - Циклом создаётся массив баттонов с тегами
+  - Циклом на каждый банно вешается onclick
+  - В каждом таком onclick-е циклом ищутся элементы с классом из innerHTML баттона
+  - Если класс есть, то элементу присваивается inline-block, если нет, то none
+*/
+
+let btnsContainer = document.getElementById("btnsContainer"),
+  btns = btnsContainer.getElementsByTagName("button"),
+  elements = document.getElementById("items");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].onclick = function() {
+    let items = elements.getElementsByClassName("all");
+
+    for (let j = 0; j < items.length; j++) {
+      if (items[j].classList.contains(this.innerHTML)) {
+        items[j].style.display = "inline-block";
+      } else {
+        items[j].style.display = "none";
+      }
+    }
+  };
+}
