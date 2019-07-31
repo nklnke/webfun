195a2d590601edb8597c36fadff1d02d171470af
Danil Kozlov <megacokes@gmail.com>
2019-07-31 19:54:10 +0500

    Update 41 React

--- udemy-jscourse/41 React/test-app/src/App.js
+++ udemy-jscourse/41 React/test-app/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Work/rest alert
+function ShowBanner(props) {
+  if (props.time > 45) {
+    return <div className="rest-block">Отдых</div>;
+  } else {
+    return <div className="work-block">Работать</div>;
+  }
 }
 
-export default App;
+/** Timer
+ * Сначала рендерится положение даты с текущим временем (вызывается метод render()).
+ * Потом вызывается метод componentDidMount, с интервалом обновления, который
+ * вызывает метод tick каждую секунду и дата обновляется.
+ * componentWillUnmpunt очистит интервал, если таймер пропадёт со страницы.
+ */
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ShowBanner time={this.state.date.getSeconds()} />
+        <h1>Current time: {this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
+}
+
+export default Clock;
--- udemy-jscourse/41 React/test-app/src/index.js
+++ udemy-jscourse/41 React/test-app/src/index.js
@@ -1,64 +1,26 @@
 import React from "react"; // Сам реакт, все его скрипты
 import ReactDOM from "react-dom"; // ОТвечает за создание dom-элементов на странице
 import "./index.css";
-import App from "./App";
+import Clock from "./App";
+import Button from "./Button";
 import * as serviceWorker from "./serviceWorker";
 
-// Hello World 1
-function Greetings1() {
-  let phrase1 = "World";
-
-  /* Внутри {} всегда строка */
-  return <h1>Hello {phrase1}</h1>;
-}
-
-// Hello World 2
-function Greetings2() {
-  return <h2>{367 + 3}</h2>;
-}
-
-// Hello World 3
-function Greetings3(props) {
-  return (
-    <h3>
-      Hello {props.name}. My name is {props.anotherName}.
-    </h3>
-  );
-}
-
-// Hello World 4
-const element = <Greetings4 phrase="SASAI LALKA" />;
-
-function Greetings4(props) {
-  return <h3>{props.phrase}.</h3>;
-}
-
-// Hello World 5
-function GreetAll() {
-  return (
-    <div>
-      <hr />
-      <Greetings4 phrase="SASAI LALKA" />
-      <Greetings4 phrase="LALKA SASAI" />
-      <Greetings4 phrase="SASAI HUYALKA" />
-      <Greetings4 phrase="HUYALKA SASAI" />
-    </div>
-  );
+// App
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Clock />
+        <Button />
+      </div>
+    );
+  }
 }
 
 // Встраивание в страницу блока с id = root, huyt, ...
-ReactDOM.render(<Greetings1 />, document.getElementById("root"));
-ReactDOM.render(<Greetings2 />, document.getElementById("huyt"));
-ReactDOM.render(
-  <Greetings3 name="Kokes" anotherName="Sasaer" />,
-  document.getElementById("third")
-);
-ReactDOM.render(element, document.getElementById("sasailalka"));
-ReactDOM.render(<GreetAll />, document.getElementById("GreetAll"));
+ReactDOM.render(<App />, document.getElementById("App"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// 17:10
