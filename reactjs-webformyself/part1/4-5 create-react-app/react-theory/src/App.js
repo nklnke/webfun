937cbea141f9185f869bcbab0d4325877d42a18b
Danil Kozlov <megacokes@gmail.com>
2019-08-02 19:54:37 +0500

    Update App.js

--- reactjs-webformyself/part1/4-5 create-react-app/react-theory/src/App.js
+++ reactjs-webformyself/part1/4-5 create-react-app/react-theory/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import Car from "./Car/Car";
 
 function App() {
+  // Можно возвращать только один корневой элемент
+  const divStyle = {
+    textAlign: "center"
+  };
+
   return (
-    <div className="App">
+    <div style={divStyle}>
       <h1>Hello World</h1>
+      <p>Hello</p>
+
+      <Car />
     </div>
   );
 }
