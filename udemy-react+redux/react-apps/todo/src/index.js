4a83159745d339e6295c24ee887d62f4f8233b2a
Danil Kozlov <megacokes@gmail.com>
2019-08-08 20:54:36 +0500

    Update index.js

--- udemy-react+redux/react-apps/todo/src/index.js
+++ udemy-react+redux/react-apps/todo/src/index.js
@@ -1,15 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input type="text" placeholder="search" />
+const TodoList = () => {
+  const items = ["Sraka", "Build pipoerdeppolo"];
+
+  return (
+    // Корнем jsx-фрагмента должен быть один элемент (один родитель)
     <ul>
       <li>Learn React</li>
       <li>Build Sosew</li>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById("root"));
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  const searchText = "Type here to search";
+  const searchStyle = {
+    fontSize: "25px"
+  };
+
+  return (
+    <input
+      style={searchStyle}
+      type="text"
+      placeholder={searchText}
+      autoComplete=""
+      tabIndex=""
+      className="class"
+      htmlFor=""
+      disabled
+    />
+  );
+};
+
+const App = () => {
+  const isLoggedIn = false,
+    loginBox = <b>Login</b>,
+    welcomeBox = <span>Welcome Back</span>;
+
+  return (
+    <div>
+      <span>{new Date().toString()}</span>
+      <br />
+      <AppHeader />
+      <SearchPanel />
+      <TodoList items={["Item 1", "Item 2"]} />
+    </div>
+  );
+};
+
+// В рендер react принимает только компоненты (не элементы)
+ReactDOM.render(<App />, document.getElementById("root"));
