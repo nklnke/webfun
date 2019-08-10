70f445cfceebd1f0b236d149abcbb0dbe0f6a3fa
Danil Kozlov <megacokes@gmail.com>
2019-08-10 19:10:25 +0500

    Replace a couple of functions to classes

--- udemy-react+redux/react-apps/todo/src/components/todo-list-item/todo-list-item.js
+++ udemy-react+redux/react-apps/todo/src/components/todo-list-item/todo-list-item.js
@@ -1,38 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./todo-list-item.css";
 
-// const TodoListItem = (props) => {
-//   return <span>{props.label}</span>;
-// };
+export default class TodoListItem extends Component {
+  render() {
+    // У класса пропсы не принимаются, а хранятся в this.props
+    const { label, important = false } = this.props;
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "steelblue" : "black",
-    fontWeight: important ? "bold" : "normal"
-  };
+    const style = {
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal"
+    };
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={style}>
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button
-        type="button"
-        className="btn btn-outline-success btn-sm float-right"
-      >
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button
-        type="button"
-        className="btn btn-outline-danger btn-sm float-right"
-      >
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
