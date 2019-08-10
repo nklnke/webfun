cee173aaef6702daf49996e5b7476f3248152f69
Danil Kozlov <megacokes@gmail.com>
2019-08-10 17:19:21 +0500

    Update components structure

---
+++ udemy-react+redux/react-apps/todo/src/components/app/app.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
+
+import "./app.css";
+
+const App = () => {
+  const todoData = [
+    { label: "Drink Coffee", important: false, id: 1 },
+    { label: "Make app", important: true, id: 2 },
+    { label: "Sasai", important: false, id: 3 }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+export default App;
