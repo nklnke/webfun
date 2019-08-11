264080bb6bfed47100f8a92827f0dd5e52befe29
Danil Kozlov <megacokes@gmail.com>
2019-08-11 22:16:48 +0500

    Add cleaning value after add item

--- udemy-react+redux/react-apps/todo/src/components/item-add-form/item-add-form.js
+++ udemy-react+redux/react-apps/todo/src/components/item-add-form/item-add-form.js
@@ -6,18 +6,25 @@ export default class ItemAddForm extends Component {
   state = {
     label: ""
   };
-  
+
   onLableChange = (event) => {
     this.setState({
+      // Здесь можно добавить условия. Например, .toUpperCase(),
+      // запрет символов, форматирование телефонов, карт, ...
       label: event.target.value
     });
   };
-  
+
   onSubmit = (event) => {
     event.preventDefault();
 
     const { onItemAdd } = this.props;
     onItemAdd(this.state.label);
+
+    // Очистка value у инпута после добавления itemа
+    this.setState({
+      label: ""
+    });
   };
 
   render() {
@@ -28,6 +35,8 @@ export default class ItemAddForm extends Component {
           className="form-control"
           onChange={this.onLableChange}
           placeholder="Input new todo item"
+          // value очищается после добавления itemа
+          value={this.state.label}
         />
         <button className="btn btn-outline-secondary">Add item</button>
       </form>
