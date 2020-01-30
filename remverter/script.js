fb047317c7acd9d9d746dc184766dfe1acd7caa4
Danil Kozlov <megacokes@gmail.com>
2020-01-30 16:43:39 +0500

    v0.1
    - convertToRem func
    - createRemTable func

--- remverter/script.js
+++ remverter/script.js
@@ -1,8 +1,32 @@
-function output(text) {
-  let output = document.getElementById("output"),
-    base = 16;
+let baseValue = document.getElementById("baseValue");
+let pxValue = document.getElementById("pxValue");
+let output = document.getElementById("output");
+let inputs = document.getElementsByTagName("input");
+let pixelColumn = document.querySelectorAll("#pxToRemTable td:first-child");
+let remColumn = document.querySelectorAll("#pxToRemTable td:nth-child(2)");
 
-  output.innerHTML = text.value / base + "rem";
+function createEventListeners() {
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("keyup", convertPxToRem);
+    inputs[i].addEventListener("keyup", createPxToRemTable);
+  }
+  createPxToRemTable();
 }
 
-// циклом по таблице пройтись - от 8 до 50-60px
+function convertPxToRem() {
+  let rem = pxValue.value / baseValue.value;
+  output.innerHTML = rem.toFixed(4) + "rem";
+}
+
+function zero(z) {}
+
+function createPxToRemTable(n) {
+  for (let i = 0; i < pixelColumn.length; i++) {
+    pixelColumn[i].innerHTML = i;
+  }
+  for (let j = 0; j < remColumn.length; j++) {
+    remColumn[j].innerHTML = j / baseValue.value + "rem";
+  }
+}
+
+createEventListeners();
