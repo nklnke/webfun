14bc3ea8a07730b1f6054d2fe4f1c87f4a4963da
Danil Kozlov <megacokes@gmail.com>
2020-05-08 20:15:55 +0500

    Create teabags.js

---
+++ temp/teabags.js
@@ -0,0 +1,11 @@
+let strength = 0;
+
+for (let i = 0; i < 50; i++) {
+    strength = strength/2 + 1;
+    console.log(strength);
+}
+
+// for (let i = 0; i < 10; i++) {
+//     strength += strength/2 + 1; // почему так-то, ептыть?
+//     console.log(strength);
+// }
